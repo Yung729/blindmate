b0ede07574cf33d6ac8a62b596038d1022ffa036
lxy0626 <xylai224@gmail.com>
2025-04-30 15:44:08 +0800

    avatar
    update ui

--- lib/views/screens/switch_avatar_screen.dart
+++ lib/views/screens/switch_avatar_screen.dart
@@ -117,67 +117,162 @@ class _SwitchAvatarScreenState extends State<SwitchAvatarScreen> {
                                   padding: const EdgeInsets.symmetric(
                                     horizontal: 8.0,
                                   ),
-                                  child: GestureDetector(
-                                    onTap: () async {
-                                      final confirm = await showDialog<bool>(
-                                        context: context,
-                                        builder:
-                                            (context) => AlertDialog(
-                                              title: const Text(
-                                                'Switch Avatar',
-                                              ),
-                                              content: const Text(
-                                                'Are you sure you want to use this avatar?',
-                                              ),
-                                              actions: [
-                                                TextButton(
-                                                  onPressed:
-                                                      () => Navigator.of(
-                                                        context,
-                                                      ).pop(true),
-                                                  child: const Text('Yes'),
-                                                ),
-                                                TextButton(
-                                                  onPressed:
-                                                      () => Navigator.of(
-                                                        context,
-                                                      ).pop(false),
-                                                  child: const Text(
-                                                    'Cancel',
-                                                    style: TextStyle(
-                                                      color: Colors.red,
+                                  child:
+                                  // child: Column(
+                                  //   // mainAxisSize: MainAxisSize.min,
+                                  //   children: [
+                                  //     GestureDetector(
+                                  //       onTap: () async {
+                                  //         final confirm = await showDialog<
+                                  //           bool
+                                  //         >(
+                                  //           context: context,
+                                  //           builder:
+                                  //               (context) => AlertDialog(
+                                  //                 title: const Text(
+                                  //                   'Switch Avatar',
+                                  //                 ),
+                                  //                 content: const Text(
+                                  //                   'Are you sure you want to use this avatar?',
+                                  //                 ),
+                                  //                 actions: [
+                                  //                   TextButton(
+                                  //                     onPressed:
+                                  //                         () => Navigator.of(
+                                  //                           context,
+                                  //                         ).pop(true),
+                                  //                     child: const Text('Yes'),
+                                  //                   ),
+                                  //                   TextButton(
+                                  //                     onPressed:
+                                  //                         () => Navigator.of(
+                                  //                           context,
+                                  //                         ).pop(false),
+                                  //                     child: const Text(
+                                  //                       'Cancel',
+                                  //                       style: TextStyle(
+                                  //                         color: Colors.red,
+                                  //                       ),
+                                  //                     ),
+                                  //                   ),
+                                  //                 ],
+                                  //               ),
+                                  //         );
+                                  //         if (confirm == true) {
+                                  //           await FirebaseFirestore.instance
+                                  //               .collection('users')
+                                  //               .doc(widget.user!.userId)
+                                  //               .update({
+                                  //                 'avatarImg': reward.imageUrl,
+                                  //               });
+                                  //           setState(() {
+                                  //             widget.user!.avatarImg =
+                                  //                 reward.imageUrl ?? '';
+                                  //           });
+                                  //         }
+                                  //       },
+                                  //       child: NetworkImageBox(
+                                  //         imageUrl: reward.imageUrl,
+                                  //         title: reward.rewardTitle,
+                                  //       ),
+                                  //     ),
+                                  //     CustomButton(
+                                  //       text:
+                                  //           isCurrentAvatar
+                                  //               ? 'Current'
+                                  //               : 'Apply',
+                                  //       onPressed:
+                                  //           isCurrentAvatar ? null : () {},
+                                  //       horizontalPadding: 12,
+                                  //       verticalPadding: 8,
+                                  //       fontSize: 12,
+                                  //       borderRadius: 20,
+                                  //       width: 80,
+                                  //     ),
+                                  //   ],
+                                  // ),
+                                  GestureDetector(
+                                    onTap:
+                                        isCurrentAvatar
+                                            ? null
+                                            : () async {
+                                              final confirm = await showDialog<
+                                                bool
+                                              >(
+                                                context: context,
+                                                builder:
+                                                    (context) => AlertDialog(
+                                                      title: const Text(
+                                                        'Switch Avatar',
+                                                      ),
+                                                      content: const Text(
+                                                        'Are you sure you want to use this avatar?',
+                                                      ),
+                                                      actions: [
+                                                        TextButton(
+                                                          onPressed:
+                                                              () =>
+                                                                  Navigator.of(
+                                                                    context,
+                                                                  ).pop(true),
+                                                          child: const Text(
+                                                            'Yes',
+                                                          ),
+                                                        ),
+                                                        TextButton(
+                                                          onPressed:
+                                                              () =>
+                                                                  Navigator.of(
+                                                                    context,
+                                                                  ).pop(false),
+                                                          child: const Text(
+                                                            'Cancel',
+                                                            style: TextStyle(
+                                                              color: Colors.red,
+                                                            ),
+                                                          ),
+                                                        ),
+                                                      ],
                                                     ),
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                      );
+                                              );
 
-                                      if (confirm == true) {
-                                        await FirebaseFirestore.instance
-                                            .collection('users')
-                                            .doc(widget.user!.userId)
-                                            .update({
-                                              'avatarImg': reward.imageUrl,
-                                            });
+                                              if (confirm == true) {
+                                                await FirebaseFirestore.instance
+                                                    .collection('users')
+                                                    .doc(widget.user!.userId)
+                                                    .update({
+                                                      'avatarImg':
+                                                          reward.imageUrl,
+                                                    });
 
-                                        setState(() {
-                                          widget.user!.avatarImg =
-                                              reward.imageUrl ?? '';
-                                        });
-                                      }
-                                    },
-                                    child: NetworkImageBox(
-                                      imageUrl: reward.imageUrl,
-                                      title: reward.rewardTitle,
+                                                setState(() {
+                                                  widget.user!.avatarImg =
+                                                      reward.imageUrl ?? '';
+                                                });
+                                              }
+                                            },
+                                    child: Column(
+                                      children: [
+                                        NetworkImageBox(
+                                          imageUrl: reward.imageUrl,
+                                          title: reward.rewardTitle,
+                                        ),
+                                        CustomButton(
+                                        text:
+                                            isCurrentAvatar
+                                                ? 'Current'
+                                                : 'Apply',
+                                        onPressed:
+                                            isCurrentAvatar ? null : () {},
+                                        horizontalPadding: 12,
+                                        verticalPadding: 8,
+                                        fontSize: 12,
+                                        borderRadius: 20,
+                                        width: 80,
+                                      ),
+                                      ],
                                     ),
                                   ),
-                          //           CustomButton(
-                          // text: isCurrentAvatar ? 'Current' : 'Apply',
-                          // onPressed: isCurrentAvatar ? null : () {
-                          //   // Do nothing when the button is tapped (or you can update the UI, etc.)
-                          // },
-                          //         ),
                                 );
                               }).toList(),
                         ),
